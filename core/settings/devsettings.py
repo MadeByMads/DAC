23f70d5167d97c129fee0a7142f745ac9119f0ad
Hasan Aliyev <hasan@onneks.com>
2020-09-15 15:36:25 +0400

    devsettings

--- core/settings/devsettings.py
+++ core/settings/devsettings.py
@@ -11,8 +11,15 @@ class DevSettings(BaseConfig):
 
     DEBUG = config("DEBUG", cast=bool, default=True)
 
-
+    DB_USER = config("DB_USER", cast=str, default="postgres")
+    DB_PASSWORD = config("DB_PASSWORD", cast=Secret, default="postgres")
+    DB_HOST = config("DB_HOST", cast=str, default="db")
+    DB_PORT = config("DB_PORT", cast=str, default="5432")
+    DB_NAME = config("DB_NAME", cast=str, default="postgres")
+    INCLUDE_SCHEMA=config("INCLUDE_SCHEMA", cast=bool, default=False)
+    
+    
     DATABASE_URL = config(
         "DATABASE_URL",
-        default="asyncpg:///test_permissions",
-    )   
+        default=f"asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}",
+    )
